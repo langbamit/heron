3e3987119d1669b94496f2123479a76427d3842f
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-02-26 19:14:52 +0100

    refactor: Update Vec3 pattern matching

--- rapier/src/convert.rs
+++ rapier/src/convert.rs
@@ -84,8 +84,8 @@ impl IntoRapier<Translation<f32>> for Vec3 {
 
 impl IntoRapier<UnitComplex<f32>> for Quat {
     fn into_rapier(self) -> UnitComplex<f32> {
-        let (Vec3 { z, .. }, angle) = self.to_axis_angle();
-        nalgebra::UnitComplex::new(if z > 0.0 { angle } else { -angle })
+        let (axis, angle) = self.to_axis_angle();
+        nalgebra::UnitComplex::new(if axis.z > 0.0 { angle } else { -angle })
     }
 }
 
